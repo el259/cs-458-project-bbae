6560a82a412d018948b0afdc624225faed1854f8
benk1378 <benjiwaffles@gmail.com>
2026-09-02 10:04:55 -0700

    Started simple quiz to json

--- nrs/create.php
+++ nrs/create.php
@@ -3,7 +3,7 @@
 
 <!--
     Ben Kanter, Blake Culbertson, Andrew Gallimore, Enrique Lopez
-    Last Modified: 8/31/26
+    Last Modified: 9/2/26
 -->
 
 <head>
@@ -15,7 +15,79 @@
 	<form method="get" action="index.html">
 		<input type="submit" value="Go Back" />
 	</form>
+<?php
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		if(isset($_POST['quizName']))
+		{
+			$quizName = htmlspecialchars($_POST['quizName']);
+			$filepath = 'quizzes/' . str_replace(' ', '_', $quizName) . '.json';
 
-	<p>Coming soon!</p>
+			if(file_exists($filepath))
+			{
+?>
+				<p>Quiz already exists. Try again</p>
+<?php
+			}
+			else
+			{
+				$questions = [ htmlspecialchars($_POST['question']) ];
+				$answers = $_POST['answer'];
+				$correct = [ $answers[0][$_POST['correct']] ];
+
+				$data = [
+					'questions' => $questions,
+					'answers' => $answers,
+					'correct' => $correct
+				];
+
+				$jsonString = json_encode($data, JSON_PRETTY_PRINT);
+
+				if(file_put_contents($filepath, $jsonString))
+				{
+?>
+					<p>Successfully created quiz!</p>
+<?php
+				}
+				else
+				{
+?>
+					<p>Error creating quiz. Try again</p>
+<?php
+				}
+			}
+		}	
+	}
+?>
+	<form method="post" action="create.php">
+		<div>
+			<label for="name">Name your quiz</label>
+			<input type="text" name="quizName" id="name" required="required" />
+		</div>
+		<div>
+			<input type="text" name="question" required="required" />
+			<div>
+				<div>
+					<input type="radio" name="correct" value="0" required="required" />
+					<input type="text" name="answer[0][]" required="required" />
+				</div>
+				<div>
+					<input type="radio" name="correct" value="1" required="required" />
+					<input type="text" name="answer[0][]" required="required" />
+				</div>
+				<div>
+					<input type="radio" name="correct" value="2" required="required" />
+					<input type="text" name="answer[0][]" required="required" />
+				</div>
+				<div>
+					<input type="radio" name="correct" value="3" required="required" />
+					<input type="text" name="answer[0][]" required="required" />
+				</div>
+			</div>
+		</div>
+		<div>
+			<input type="submit" value="Create" />
+		</div>
+	</form>
 </body>
 </html>
--- nrs/index.js
+++
@@ -1,6 +0,0 @@
-/*
- * Andrew Gallimore, Blake Culbertson, Ben Kanter, Enrique Lopez
- * Last modified: 9/1/26
- */
-
-console.log("The script is successfully connected!");
